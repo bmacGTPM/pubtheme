93a6ead5000358df3ae753fd4986b3fba36f702d
Brian Macdonald <bmacgtpm@gmail.com>
2024-01-11 16:36:53 -0500

    add documentation

--- R/reverse2_trans.r
+++ R/reverse2_trans.r
@@ -1,6 +1,7 @@
-## Function for reverse date axes 
-## Copied from https://github.com/tidyverse/ggplot2/issues/4014
-## Use this with scale_y_continuous below (and use _continuous instead of _date)
+#' Function for reverse date axes 
+#' 
+#' Copied from https://github.com/tidyverse/ggplot2/issues/4014
+#' Use this with scale_y_continuous below (and use _continuous instead of _date)
 #' @export
 reverse2_trans <- function() {
   trans_new(
